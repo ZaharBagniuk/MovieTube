b47a818ef317b5e554f5a6e7641a16207486164c
zbahniuk <zbahniuk>
2022-04-29 21:07:59 +0100

    Adjust config

--- movie-tube/cypress/integration/moviesList.spec.ts
+++ movie-tube/cypress/integration/moviesList.spec.ts
@@ -1,13 +1,20 @@
 const localHost = 'http://localhost:3000/';
-const baseUrl = 'https://api.themoviedb.org/3';
 const query = 'Spy';
-const API_KEY = '251e8e9e47e44eddef1cf3e04d19caab';
 
 describe('Movies List', () => {
     it('Visits the dashboard and shows top-rated results', () => {
         cy.visit(localHost);
         cy.get('[data-testid="MoviesList"]').find('[data-testid="MovieCard"]').should('have.length', 20);
         cy.get('[data-testid="resultsHeader"]').should('have.text', 'Most Rated of All Time');
+    });
+
+    it('Selects movie from search bar', () => {
+        cy.visit(localHost);
+        cy.get('[data-testid="searchInputs"] [data-testid="input"]').type(query);
+        cy.get('[data-testid="dataResult"]').find('[data-testid="dataItem"]').should('have.length', 20);
+        cy.get('[data-testid="dataResult"] [data-testid="dataItem"]:first-child').click();
+        cy.url().should('include', `${localHost}movie/`);
+        cy.get('[data-testid="generalInfoContainer"] [data-testid="detailsSection"] [data-testid="Title"]').should('exist');
     });
 
     it('Redirects back to the dashboard from selected movie', () => {
@@ -31,20 +38,6 @@ describe('Movies List', () => {
         cy.get('[data-testid="HomeBtn"]').click();
         cy.url().should('eq', localHost);
         cy.get('[data-testid="resultsHeader"]').should('have.text', 'Most Rated of All Time');
-    });
-
-    it('Shows Loader for movies list', () => {
-        cy.intercept(
-            'GET',
-            `${baseUrl}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
-            {
-                fixture: 'movies'
-            }
-        ).as('topRatedMoviesSearch');
-        cy.visit(localHost);
-        cy.get('[data-testid="Loader"]').should('exist');
-        cy.wait(1000);
-        cy.wait('@topRatedMoviesSearch');
     });
 
     it('Allows to visit movies page by direct link', () => {
